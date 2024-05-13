9b02c89721c130a5e153354a3c251386e08075bc
mailcorahul <mailcorahul@gmail.com>
2024-05-13 13:36:51 +0000

    Add VLM support - llava-next - init

--- image_classification/model.py
+++ image_classification/model.py
@@ -1,4 +1,6 @@
 from config import IMAGE_CLASSIFIER_CONFIG
+
+from transformers import AutoProcessor, LlavaNextForConditionalGeneration
 import open_clip
 
 class Models:
@@ -23,6 +25,21 @@ class Models:
 
         return model_data
 
+    def create_llava_next_arch(self):
+
+        model = LlavaNextForConditionalGeneration.from_pretrained(
+            "llava-hf/llava-v1.6-mistral-7b-hf")
+        processor = AutoProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+        prompt = "[INST] <image>\nWhat is shown in this image? [/INST]"
+
+        model_data = {
+            "model": model,
+            "processor": processor,
+            "prompt": prompt
+        }
+
+        return model_data
+
     def create_model(self):
         """Instantiate the network class and initialize with pretrained weights."""
 
@@ -36,3 +53,6 @@ class Models:
 
         elif model_arch == "blip2":
             pass
+
+        elif model_arch == "LLaVA-NeXT":
+            return self.create_llava_next_arch()
